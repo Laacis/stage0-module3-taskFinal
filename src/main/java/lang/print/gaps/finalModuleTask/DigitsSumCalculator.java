0e2bf41ec93087e370ade593b85c26c8385ea1e0
Jurijs <j.lacis.code@gmail.com>
2023-12-17 12:29:28 +0800

    complete final

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,13 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int first, second, third, fourth;
+
+        first = number % 10;
+        second = (number % 100) /10;
+        third = (number % 1000) /100;
+        fourth = (number % 10000) / 1000;
+
+        System.out.println(first+ second+third+fourth);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,10 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int first, second, third;
+        first = (number % 10)*100;
+        second = (number % 100) - (number % 10);
+        third = ((number % 1000) - (number % 100)) /100;
+        System.out.println(first + second + third);
     }
 }
